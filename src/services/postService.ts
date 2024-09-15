3b921d820330ba92dc7cf9d57bec11075e0019dc
jmoraba_bci <jhan.mora@bci.cl>
2024-09-14 21:56:40 -0300

    feat: Update post title and author in db.json, implement post update functionality, and add lazy loading for images

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -14,7 +14,7 @@ export class PostService {
     perPage,
   }: Paginate): Promise<PostResponse> {
     try {
-      await sleep(5000);
+      await sleep(500);
       const response = await api.get<PostResponse>(
         `/posts?_page=${page}&_per_page=${perPage}`,
       );
@@ -27,7 +27,7 @@ export class PostService {
 
   public static async getPostById(postId: string): Promise<Post> {
     try {
-      await sleep(5000);
+      await sleep(500);
       const response = await api.get<Post>(`/posts/${postId}`);
       return response.data;
     } catch (error) {
@@ -38,7 +38,7 @@ export class PostService {
 
   public static async createPost(post: Omit<Post, 'id'>): Promise<Post> {
     try {
-      await sleep(5000);
+      await sleep(500);
       const validPost = PostSchema.parse(post);
       const response = await api.post<Post>('/posts', validPost);
       return response.data;
@@ -50,7 +50,7 @@ export class PostService {
 
   public static async updatePost(post: Post): Promise<Post> {
     try {
-      await sleep(5000);
+      await sleep(500);
       const validPost = PostSchema.parse(post);
       const response = await api.put<Post>(`/posts/${post?.id}`, validPost);
       return response.data;
@@ -62,7 +62,7 @@ export class PostService {
 
   public static async deletePost(postId: string): Promise<void> {
     try {
-      await sleep(1000);
+      await sleep(500);
       await api.delete(`/posts/${postId}`);
     } catch (error) {
       console.error('Error in deletePost method', error);
