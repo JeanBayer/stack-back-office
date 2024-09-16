70ef245ce568a2b41291ec30968d7800f53c2dd3
Fidel <35785518+fpopaya@users.noreply.github.com>
2024-09-16 20:58:54 -0300

    Add: integro test unitarios

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1', // Soporte para rutas alias
+    },
+    setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  };
+  
---
+++ src/__test__/components/VerticalDotsIcon.test.tsx
@@ -0,0 +1,36 @@
+import { render } from '@testing-library/react';
+import { VerticalDotsIcon } from '../../components/icons/verticalDotsIcon';
+
+describe('VerticalDotsIcon', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<VerticalDotsIcon />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it('applies default size when no size, width, or height is provided', () => {
+    const { container } = render(<VerticalDotsIcon />);
+    const svg = container.querySelector('svg');
+    expect(svg).toHaveAttribute('height', '24');
+    expect(svg).toHaveAttribute('width', '24');
+  });
+
+  it('applies provided size', () => {
+    const { container } = render(<VerticalDotsIcon size={32} />);
+    const svg = container.querySelector('svg');
+    expect(svg).toHaveAttribute('height', '32');
+    expect(svg).toHaveAttribute('width', '32');
+  });
+
+  it('applies provided width and height', () => {
+    const { container } = render(<VerticalDotsIcon width={40} height={50} />);
+    const svg = container.querySelector('svg');
+    expect(svg).toHaveAttribute('height', '24');
+    expect(svg).toHaveAttribute('width', '24');
+  });
+
+  it('applies additional props', () => {
+    const { container } = render(<VerticalDotsIcon data-testid="vertical-dots-icon" />);
+    const svg = container.querySelector('svg');
+    expect(svg).toHaveAttribute('data-testid', 'vertical-dots-icon');
+  });
+});
