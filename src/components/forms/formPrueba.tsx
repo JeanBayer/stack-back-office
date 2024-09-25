010804676583bdd2065314e2d163ae5bc4ef6835
jmoraba_bci <jhan.mora@bci.cl>
2024-09-25 19:32:04 -0300

    prueba: agrego form de prueba

---
+++ src/components/forms/formPrueba.tsx
@@ -0,0 +1,147 @@
+import { Filter, FilterSchema, Status } from '@/types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Button,
+  Input,
+  Select,
+  SelectItem,
+  Spacer,
+  Spinner,
+} from '@nextui-org/react';
+import { useForm, UseFormRegister } from 'react-hook-form';
+
+interface FormFilterProps {
+  filter?: Filter;
+  status: Status[];
+  onSubmit: (updatedPost: Filter) => void;
+  isDisabledButton?: boolean;
+  isSubmitting?: boolean;
+}
+
+const Monto = ({ ...props }) => {
+  return (
+    <Input
+      label="Monto"
+      placeholder="Search by monto"
+      className="max-w-xs"
+      {...props}
+    />
+  );
+};
+
+interface Option {
+  label: string;
+  id: string;
+  tipo: string;
+  properties: {
+    message: string;
+    placeholder: string;
+    tipo: {
+      id: 'monto-mision-instruccion-monto-mision';
+      placeholder: string;
+    };
+  };
+}
+
+export const Factory = ({
+  register,
+  tipo,
+  option,
+}: {
+  register: UseFormRegister<Filter>;
+  tipo: string;
+  option: Option;
+}) => {
+  console.log('option', option);
+  const DICT: { [key: string]: JSX.Element } = {
+    monto: <Monto register={register} />,
+  };
+  if (!tipo) return null;
+
+  if (option.tipo === 'instruccion-monto') {
+    const subProperties = option.properties.tipo;
+
+    return (
+      <Monto
+        placeholder={subProperties.placeholder}
+        {...register(subProperties.id)}
+      />
+    );
+  }
+
+  if (!DICT[tipo]) return null;
+
+  return DICT[tipo];
+};
+
+const option: Option = {
+  label: 'Misión con meta de compra (llegar a un monto de compra)',
+  id: 'instruccion-monto-mision',
+  tipo: 'instruccion-monto',
+  properties: {
+    message: 'title ...',
+    placeholder: 'Instrucción',
+    tipo: {
+      id: 'monto-mision-instruccion-monto-mision',
+      placeholder: 'Monto de compra',
+    },
+  },
+};
+
+export const FormPrueba = ({
+  filter,
+  status,
+  onSubmit,
+  isDisabledButton = false,
+  isSubmitting,
+}: FormFilterProps) => {
+  const {
+    watch,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Filter>({
+    resolver: zodResolver(FilterSchema),
+    mode: 'all', // muestre los errores en onchange, blur y submit
+    criteriaMode: 'all', // muestre todos los inputs con error
+    defaultValues: filter,
+  });
+
+  const disabledButton = isDisabledButton || Object.keys(errors).length > 0;
+
+  const optionEstado = watch('estado');
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="text-gray-900">
+        <Select
+          items={status}
+          label="State"
+          placeholder="Select an State"
+          className="max-w-xs"
+          {...register('estado')}
+        >
+          {(estado) => <SelectItem key={estado.key}>{estado.label}</SelectItem>}
+        </Select>
+        {/* NUEVO COMPONENTE */}
+        {Factory({ register, tipo: optionEstado, option: option })}
+        {/* NUEVO COMPONENTE */}
+
+        <Spacer y={1} />
+        <Input
+          label="Title"
+          placeholder="Search by title"
+          className="max-w-xs"
+          {...register('title')}
+          isInvalid={!!errors.title}
+          errorMessage={errors.title?.message}
+        />
+        <Spacer y={1} />
+        <Button type="submit" color="primary" isDisabled={disabledButton}>
+          Search
+          {isSubmitting && <Spinner size="sm" color="secondary" />}
+        </Button>
+      </form>
+    </div>
+  );
+};
--- src/types/filter.ts
+++ src/types/filter.ts
@@ -3,9 +3,11 @@ import { z } from 'zod';
 export interface Filter {
   estado: string;
   title: string;
+  'monto-mision-instruccion-monto-mision': string;
 }
 
 export const FilterSchema = z.object({
   estado: z.string().optional(),
   title: z.union([z.string().min(3).max(255), z.string().length(0)]).optional(),
+  'monto-mision-instruccion-monto-mision': z.string().optional(),
 });
