3b4096f9a5e7061a55f6632f54661a9168864e04
Fidel <fidel.popayan@bci.cl>
2024-09-27 21:34:17 -0300

    feat: Agregar componente CSVForm para manejar la entrada de archivos CSV en FormPrueba

--- src/components/forms/prueba/CSVForm.tsx
+++ src/components/forms/prueba/CSVForm.tsx
@@ -1,11 +1,41 @@
+import { useState, useEffect, useCallback } from 'react';
 import { FormComponentProps } from '@/types';
 import { Input } from '@nextui-org/react';
 
 export const CSVForm = ({ register, option }: FormComponentProps) => {
-  if (!option || !option?.properties?.id) return null;
-  const { placeholder, id } = option.properties;
+  const [csvContent, setCsvContent] = useState('');
+
+  const { placeholder = '', id = '' } = option?.properties || {};
+
+  useEffect(() => {
+    if (id && csvContent) {
+      register(id, { value: csvContent });
+    }
+  }, [csvContent, id, register]);
+
+  const handleFileChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const content = event.target?.result as string;
+        if (content) setCsvContent(content.trim());
+      };
+      reader.onerror = () => {
+        console.error('Error  bb');
+      };
+      reader.readAsText(file);
+    }
+  }, []);
 
   return (
-    <Input placeholder={placeholder} className="max-w-xs" {...register(id)} />
+    <Input
+      type="file"
+      placeholder={placeholder}
+      className="max-w-xs"
+      accept=".csv"
+      onChange={handleFileChange}
+    />
   );
 };
